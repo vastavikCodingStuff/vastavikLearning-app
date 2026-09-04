a74ad6e5e1e46415817d1d1d27cff7c5b68d8298
Parth Vastavik <131694386+parthasdey2304@users.noreply.github.com>
2026-09-04 07:36:57 +0530

    chore: bump version to 1.0.10 (versionCode 11)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.vastavik.computer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.9"
+        versionCode = 11
+        versionName = "1.0.10"
         multiDexEnabled = true
         vectorDrawables {
             useSupportLibrary = true
