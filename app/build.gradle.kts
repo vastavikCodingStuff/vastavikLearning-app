112b6cd8fd882691bb8eb9530dc4a6c0443757c2
Parth Vastavik <131694386+parthasdey2304@users.noreply.github.com>
2026-09-04 06:50:45 +0530

    chore: bump version to 1.0.8 (versionCode 9)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.vastavik.computer"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "1.0.7"
+        versionCode = 9
+        versionName = "1.0.8"
         multiDexEnabled = true
         vectorDrawables {
             useSupportLibrary = true
